418ddb77a5a69cb5970e90668a26fad960e7c43b
Vyonavee <vyonanjeri62@gmail.com>
2025-10-25 19:28:59 +0300

    fix :tailwind deployment

--- task-manager-app/vite.config.mjs
+++ task-manager-app/vite.config.mjs
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // important for correct asset resolution on Vercel
+  build: {
+    outDir: "dist",
+  },
+  base: "./", // this is important
 });
