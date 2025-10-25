17bfa8fcb22dd884d97b74bf907622818c632de4
Vyonavee <vyonanjeri62@gmail.com>
2025-10-25 18:18:47 +0300

    fixed vercel hosting error 404

--- task-manager-app/vite.config.mjs
+++ task-manager-app/vite.config.mjs
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/react-js-jsx-and-css-mastering-front-end-development-Vyonavee/", // important for GitHub Pages
   build: {
     outDir: "dist",
   },
-  server: {
-    port: 5173,
-  },
-  base: "./", // <-- important for Vercel hosting
 });
