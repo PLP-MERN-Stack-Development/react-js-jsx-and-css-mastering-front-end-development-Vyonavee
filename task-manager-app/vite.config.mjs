148faa8395e34d9d9ab0b4183111c4d0cc184cd0
Vyonavee <vyonanjeri62@gmail.com>
2025-10-25 18:52:48 +0300

    updated package.json

--- task-manager-app/vite.config.mjs
+++ task-manager-app/vite.config.mjs
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-js-jsx-and-css-mastering-front-end-development-Vyonavee/", // important for GitHub Pages
-  build: {
-    outDir: "dist",
-  },
+  base: "/", // important for correct asset resolution on Vercel
 });
